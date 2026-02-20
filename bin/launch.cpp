ddb67f870928a180acf343973a5949fe346d57e4
Vladimir Ivanov <ivvl82@gmail.com>
2026-02-20 18:23:17 +0300

    Make code in launch.cpp a little bit clear.

--- bin/launch.cpp
+++ bin/launch.cpp
@@ -48,55 +48,57 @@ int main(int argc, char *argv[]) {
   CUcontext context;
   CUDA_CHECK(cuCtxCreate(&context, 0, device));
 
-  // Load kernel
+  // Load a kernel
   CUmodule module;
   CUDA_CHECK(cuModuleLoad(&module, ptxFile.c_str()));
   CUfunction function;
   CUDA_CHECK(cuModuleGetFunction(&function, module, funcName.c_str()));
 
+  // Allocate device memory
+  CUdeviceptr dev_A, dev_B, dev_C;
+  CUDA_CHECK(cuMemAlloc(&dev_A, bytes));
+  CUDA_CHECK(cuMemAlloc(&dev_B, bytes));
+  CUDA_CHECK(cuMemAlloc(&dev_C, bytes));
+
+  // Initialize a stream to execute the kernel
   CUstream stream;
   CUDA_CHECK(cuStreamCreate(&stream, CU_STREAM_NON_BLOCKING));
 
-  // Allocate memory for arrays A, B, and C on host
-  std::vector<double> h_A(N), h_B(N), h_C(N);
-  // Fill host arrays A and B
+  // Fill arrays on the host
+  std::vector<double> host_A(N), host_B(N);
   for (int i = 0; i < N; ++i) {
-    h_A[i] = i;
-    h_B[i] = 2 * i;
+    host_A[i] = i;
+    host_B[i] = 2 * i;
   }
 
-  // Allocate memory for arrays d_A, d_B, and d_C on device
-  CUdeviceptr d_A, d_B, d_C;
-  CUDA_CHECK(cuMemAlloc(&d_A, bytes));
-  CUDA_CHECK(cuMemAlloc(&d_B, bytes));
-  CUDA_CHECK(cuMemAlloc(&d_C, bytes));
+  // Copy data to the device
+  CUDA_CHECK(cuMemcpyHtoDAsync(dev_A, host_A.data(), bytes, stream));
+  CUDA_CHECK(cuMemcpyHtoDAsync(dev_B, host_B.data(), bytes, stream));
 
-  // Copy data from host arrays A and B to device arrays d_A and d_B
-  CUDA_CHECK(cuMemcpyHtoDAsync(d_A, h_A.data(), bytes, stream));
-  CUDA_CHECK(cuMemcpyHtoDAsync(d_B, h_B.data(), bytes, stream));
-
-  // Launch kernel
-  void *args[] = {&d_A, &d_B, &d_C, (void *)&N};
+  // Launch the kernel
+  void *args[] = {&dev_A, &dev_B, &dev_C, (void *)&N};
   CUDA_CHECK(cuLaunchKernel(function,  //
                             1, 1, 1,   // gridDim
                             256, 1, 1, // blockDim
                             0, stream, args, nullptr));
 
-  // Copy data from device array d_C to host array C
-  CUDA_CHECK(cuMemcpyDtoHAsync(h_C.data(), d_C, bytes, stream));
+  // Copy the return data back
+  std::vector<double> host_C(N);
+  CUDA_CHECK(cuMemcpyDtoHAsync(host_C.data(), dev_C, bytes, stream));
 
   CUDA_CHECK(cuStreamSynchronize(stream));
   CUDA_CHECK(cuStreamDestroy(stream));
 
   // Cleanup
-  CUDA_CHECK(cuMemFree(d_A));
-  CUDA_CHECK(cuMemFree(d_B));
-  CUDA_CHECK(cuMemFree(d_C));
+  CUDA_CHECK(cuMemFree(dev_A));
+  CUDA_CHECK(cuMemFree(dev_B));
+  CUDA_CHECK(cuMemFree(dev_C));
   CUDA_CHECK(cuModuleUnload(module));
   CUDA_CHECK(cuCtxDestroy(context));
 
   for (int i = 0; i < N; ++i)
-    std::cout << h_A[i] << " + " << h_B[i] << " = " << h_C[i] << std::endl;
+    std::cout << host_A[i] << " + " << host_B[i] << " = " << host_C[i]
+              << std::endl;
 
   return EXIT_SUCCESS;
 }
